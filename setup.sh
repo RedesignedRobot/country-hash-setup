51d7474eee1a7bf0b2827de08a0670ae4a95642c
Amir Ayub <dev.amirayub@gmail.com>
2019-07-27 11:25:54 +0300

    fixed script

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ docker run -d -p 5000:5000 --name service amirayub/hash-rest-service
 docker run -d -p 80:80 --name client amirayub/secure-hash-client
 docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
 docker exec mongodb apt update
-docker exec mongodb apt upgrade
-docker exec mongodb apt install git
+docker exec mongodb apt upgrade -y
+docker exec mongodb apt install git -y
 docker exec mongodb cd home/
 docker exec mongodb git clone https://github.com/ozlerhakan/mongodb-json-files.git
 docker exec mongodb cd mongodb-json-files/datasets/
