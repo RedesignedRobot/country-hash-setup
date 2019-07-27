a5bd53d0d1996fee597823d13cddf40261f7d3d5
Amir Ayub <dev.amirayub@gmail.com>
2019-07-27 13:01:14 +0300

    fixed stuff...

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 apt update
 apt upgrade -y
 apt install mongodb-clients -y
+apt install vim -y
 docker run -d -p 5000:5000 --name service amirayub/hash-rest-service
 docker run -d -p 80:80 --name client amirayub/secure-hash-client
 docker run -d -p 27017-27019:27017-27019 --name mongodb mongo
